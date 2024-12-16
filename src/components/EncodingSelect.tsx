37b66fab975b1987e71955ff8584c123e12b46b9
API2 <164052842+long2005a1@users.noreply.github.com>
2024-12-16 10:53:32 +0000

    壮举：自动检测文本预览的编码 （#204)

--- src/components/EncodingSelect.tsx
+++ src/components/EncodingSelect.tsx
@@ -1,9 +1,12 @@
 import { Box } from "@hope-ui/solid"
 import { SelectWrapper } from "./Base"
+import chardet from "chardet"
+import { createEffect } from "solid-js"
 
 export function EncodingSelect(props: {
   encoding: string
   setEncoding: (encoding: string) => void
+  referenceText?: string | ArrayBuffer
 }) {
   const encodingLabels = [
     "utf-8",
@@ -14,6 +17,25 @@ export function EncodingSelect(props: {
     "utf-16be",
     "utf-16le",
   ]
+
+  createEffect(() => {
+    if (props.referenceText) {
+      let buffer: Uint8Array
+      if (typeof props.referenceText === "string") {
+        buffer = new TextEncoder().encode(props.referenceText)
+      } else {
+        buffer = new Uint8Array(props.referenceText)
+      }
+      for (let encoding of chardet.analyse(buffer)) {
+        const encodingLabel = encoding.name.toLowerCase()
+        if (encodingLabels.includes(encodingLabel)) {
+          props.setEncoding(encodingLabel)
+          return
+        }
+      }
+    }
+  })
+  
   return (
     <Box
       pos="absolute"
