74652e3f30d6218ab865e569d729b6c46c533f7e
API2 <164052842+long2005a1@users.noreply.github.com>
2025-02-03 14:59:48 +0000

    修复（web）：离开视频页面后取消视频加载 （#221)

--- src/pages/home/previews/video.tsx
+++ src/pages/home/previews/video.tsx
@@ -285,6 +285,7 @@ const Preview = () => {
     })
   })
   onCleanup(() => {
+    if (player && player.video) player.video.src = ""
     player?.destroy()
   })
   const [autoNext, setAutoNext] = createSignal()
